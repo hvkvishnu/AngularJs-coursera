f328884db4deb3eae5fa6a3ce500913fbfe281b9
vishnu kumar <vishnu@localhost>
2020-07-24 21:49:35 +0530

    NameCalculator App

---
+++ app.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+
+  angular.module("NameCalculator",[])
+  .controller('NameCalculatorController',function($scope){
+
+    $scope.name = '';
+    $scope.totalvalue = 0;
+
+    $scope.displayNumeric = function () {
+      var totalNameValue = calculateNumericForString($scope.name);
+      $scope.totalvalue = totalNameValue;
+
+    };
+
+    function calculateNumericForString(string) {
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
+    }
+
+
+  });
+})();
